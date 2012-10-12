9df605e0bafe81024adc549c6d8f5da8d7ffe55c
Dwayne Charrington <dwayencharrington@gmail.com>
2012-10-12 15:47:32 +1000

    Insert function slugifies the name.

--- App/application/modules/project/models/project_m.php
+++ App/application/modules/project/models/project_m.php
@@ -39,7 +39,10 @@ class Project_m extends CI_Model {
 	}
 
 	public function insert($data)
-	{
+	{	
+		// Create a slug
+		$data['slug'] = url_title($data['name'], '-', TRUE);
+
 		// Insert into database
 		$this->db->insert('projects', $data);
 	}
