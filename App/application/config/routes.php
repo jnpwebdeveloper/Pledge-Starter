25b0a4cec30fe16c0c4c0c730ee65e210f32a92a
Dwayne Charrington <dwayencharrington@gmail.com>
2012-10-12 15:11:57 +1000

    Default route set to the project controller.

--- App/application/config/routes.php
+++ App/application/config/routes.php
@@ -1,22 +1,15 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$route['default_controller']  = 'welcome';
+$route['default_controller']  = 'project';
 
 $route['login'] = "users/login";
 
 $route['paypal/process/(:any)'] = 'transaction/process/$1';
 
-$route['yard/(:num)']            = "yards/view/$1";
-$route['yard/(:num)/make_offer'] = "yards/make_offer";
-
 // Admin routes
 $route['admin'] = "admin";
 $route['admin/([a-zA-Z_-]+)/(:any)'] = "$1/admin/$2";
 $route['admin/([a-zA-Z_-]+)'] = "$1/admin/index";
-
-// Dashboard routes
-$route['dashboard'] = "users/dashboard";
-$route['dashboard/(:any)'] = "users/dashboard/$1";
 
 $route['404_override'] = '';
 
